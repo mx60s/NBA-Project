84c3c947fa02de37fbfce53eae5c8af02a52e88d
Maggie von Ebers <vonebersm@gmail.com>
2019-10-08 14:31:11 -0500

    ok

--- StatsScraper.py
+++ StatsScraper.py
@@ -9,7 +9,7 @@ chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_argument("headless")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
-#Get name from draftFile
+# Get name from draftFile
 statsFile = open("statsFile.csv", "w+")
 linksFile = open("PlayerLinks.csv")
 linksReader = csv.reader(linksFile)
@@ -38,24 +38,25 @@ tables = [perGameCol, advancedCol]
 for table in tables:
     for col in table.find_elements_by_xpath(".//th"):
         label = col.get_attribute('data-stat')
-        default_fields[label] = "NA"
+        default_fields[label] = 0
+
+default_fields["name"] = "NA"
 
 csv_header = ""
 for field in default_fields.keys():
     csv_header += field + ", "
 
-#print(csv_header)
+# print(csv_header)
 
 statsFile.write(csv_header + "\n")
 
-#print(*default_fields.keys())
+# print(*default_fields.keys())
 
 """
 Iterate through players and collect values
 """
 try:
     for player, link in linksReader:
-
         try:
             driver.get(link)
         except:
@@ -65,32 +66,37 @@ try:
             driver.get(link)
 
         perGame = driver.find_element_by_xpath('//table[@id="per_game"]/tbody')
-        advanced = driver.find_element_by_xpath('//table[@id="advanced"]/tbody')
+        advanced = driver.find_element_by_xpath(
+            '//table[@id="advanced"]/tbody')
 
         pg_years = perGame.find_elements_by_xpath(".//tr")
         a_years = advanced.find_elements_by_xpath(".//tr")
 
-        for i in range(len(pg_years)):  # for each year a player is active, get row data for that year from both tables
-            playerData = copy.deepcopy(default_fields) # start off with default fields
-            playerData["season"] = pg_years[i].find_element_by_xpath(".//th").find_element_by_xpath(".//a").text
+        # for each year a player is active, get row data for that year from both tables
+        for i in range(len(pg_years)):
+            # start off with default fields
+            playerData = copy.deepcopy(default_fields)
+            playerData["season"] = pg_years[i].find_element_by_xpath(
+                ".//th").find_element_by_xpath(".//a").text
             for table in [pg_years, a_years]:
                 for col in table[i].find_elements_by_xpath(".//td"):
                     label = col.get_attribute('data-stat')
+                    if not label:
+                        continue
                     try:
                         if col.find_element_by_xpath(".//a").text:
-                            playerData[label] = col.find_element_by_xpath(".//a").text
+                            playerData[label] = col.find_element_by_xpath(
+                                ".//a").text
                     except:
                         if col.text:
                             playerData[label] = col.text
-                
-                    playerData[label] = col.text
-        
+
             playerData["name"] = player
 
             csv_line = ""
             for val in playerData.values():
-                csv_line += val + ","
-            #print(csv_line)
+                csv_line += str(val) + ", "
+            # print(csv_line)
             statsFile.write(csv_line + "\n")
 
 except:
