80ac0d1302d87b0b17bb0ce7f5ff75dc5c79f6af
Maggie von Ebers <vonebersm@gmail.com>
2019-10-08 13:12:10 -0500

    Revert "Merge branch 'master' of https://github.com/mx60s/NBA-Project"
    This reverts commit 24e0c205ef14205f9f605472d72db78ffbdf4883, reversing
    changes made to f3ccdb4e6fe8d0d19e40bb1404a5f7734637485a.

--- playerProfileLink.py
+++ playerProfileLink.py
@@ -2,27 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import csv
 import string
-import unidecode
-
 driver = webdriver.Chrome()
 
-with open("PlayerLinks.csv", "w+") as linkFile:
+with open("./Desktop/PlayerLinks.csv", "w+") as file:
     for char in string.ascii_lowercase:
         driver.get("https://www.basketball-reference.com/players/" + char)
 
+        #if table needs to be expanded, find element and clicks on it
+        exists = True
+        try:
+            driver.find_element_by_id("players_control")
+        except:
+            exists = False
+        if exists == True:
+            driver.find_element_by_id("players_control").click()
+
         playerTable = driver.find_elements_by_xpath('//*[@id="players"]/tbody/tr')
         for line in playerTable:
-            if (line.get_attribute("class") != "thead"):
-                name = line.find_element_by_xpath(".//th").text
-                nameFix = unidecode.unidecode(name.replace("*", ""))
-                link = line.find_element_by_css_selector('a').get_attribute('href')
-                #print(nameFix)
-                #print(link)
-                linkFile.write(nameFix + ", " + link + "\n")
-
-linkFile.close()
+            name = line.find_element_by_xpath(".//th").text
+            nameFix = name.replace("*", "")
+            link = line.find_element_by_css_selector('a').get_attribute('href')
+            file.write(nameFix + ", " + link + "\n")
+file.close()
 driver.close()
-
-
-
-
